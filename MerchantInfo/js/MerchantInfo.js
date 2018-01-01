3e6416aa15f4f58e8ff8476b01893f7f4b07c9d3
九月 <2245729247@qq.com>
2018-01-01 14:46:57 +0800

    Merchant Personal Info

---
+++ MerchantInfo/js/MerchantInfo.js
@@ -0,0 +1,60 @@
+	function userInfo(){
+				var odiv2=document.getElementById('div2');
+				var odiv3=document.getElementById('div3');
+				var odiv4=document.getElementById('div4');
+				var odiv5=document.getElementById('div5');
+				var odiv6=document.getElementById('div6');
+				odiv2.style.display='block';
+				odiv3.style.display='none';
+				odiv4.style.display='none';	
+				odiv5.style.display='none';
+				odiv6.style.display='none';
+			}
+			function pwdChange(){
+				var odiv2=document.getElementById('div2');
+				var odiv3=document.getElementById('div3');
+				var odiv4=document.getElementById('div4');
+				var odiv5=document.getElementById('div5');
+				var odiv6=document.getElementById('div6');
+				odiv4.style.display='block';
+				odiv2.style.display='none';
+				odiv3.style.display='none';
+				odiv5.style.display='none';
+				odiv6.style.display='none';
+			}
+			function infoChange(){	
+			    var odiv2=document.getElementById('div2');
+				var odiv3=document.getElementById('div3');
+				var odiv4=document.getElementById('div4');
+				var odiv5=document.getElementById('div5');
+				var odiv6=document.getElementById('div6');			
+				odiv3.style.display='block';
+				odiv4.style.display='none';
+				odiv2.style.display='none';
+				odiv5.style.display='none';
+				odiv6.style.display='none';
+			}
+			function MedDiet(){	
+			    var odiv2=document.getElementById('div2');
+				var odiv3=document.getElementById('div3');
+				var odiv4=document.getElementById('div4');
+				var odiv5=document.getElementById('div5');
+				var odiv6=document.getElementById('div6');			
+				odiv5.style.display='block';
+				odiv2.style.display='none';
+				odiv3.style.display='none';
+				odiv4.style.display='none';
+				odiv6.style.display='none';
+			}
+			function MedMater(){
+			    var odiv2=document.getElementById('div2');
+				var odiv3=document.getElementById('div3');
+				var odiv4=document.getElementById('div4');
+				var odiv5=document.getElementById('div5');
+				var odiv6=document.getElementById('div6');				
+				odiv6.style.display='block';
+				odiv2.style.display='none';
+				odiv3.style.display='none';
+				odiv4.style.display='none';
+				odiv5.style.display='none';
+			}
